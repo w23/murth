3fd7cf090af01effafcb3e159ea7b49aa9cc2b72
Ivan 'w23' Avdeev <me@w23.ru>
2013-03-28 03:10:29 +0700

    the forgotten test!

---
+++ murth/test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "synth.h"
+
+const char *assembly =
+"54 inote2fdeltaphase $delta storeglobal pop\n" // initial delta_phase
+"00 :phasemod spawn\n" // spawn phasemod "thread"
+"forever:\n" // main synthloop
+  "$phase loadglobal $delta loadglobal fadd fphaserot $phase storeglobal fsin 00 storeglobal pop\n"
+  "00 idle :forever jmp\n"
+"phasemod:\n" // second "thread"
+  "$delta loadglobal 0.997 fmul $delta storeglobal pop\n"
+  "04 idle :phasemod jmp\n";
+
+#define SAMPLES 8192
+float samples[SAMPLES];
+
+int main(int argc, char *argv[])
+{
+  murth_init(assembly, 44100, 90);
+  murth_synthesize(samples, SAMPLES);
+
+  FILE *f = fopen("synth.data", "w");
+  for (int i = 0; i < SAMPLES; ++i)
+    fprintf(f, "%f\n", samples[i]);
+  fclose(f);
+
+  return 0;
+}
