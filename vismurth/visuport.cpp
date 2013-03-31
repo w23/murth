c89e9d1119eea0a2dd165e2a5fafc504da43a63f
Ivan 'w23' Avdeev <me@w23.ru>
2013-03-31 12:57:51 +0700

    sequencer from tech preview

--- vismurth/visuport.cpp
+++ vismurth/visuport.cpp
@@ -14,6 +14,9 @@ public:
   virtual void resize(vec2i);
   virtual void draw(int ms, float dt);
   virtual void close();
+  virtual void inputKey(const KeyState &ks) {
+    controller_->quit(0);
+  }
 private:
   IViewportController *controller_;
   bool is_playing_;
@@ -24,11 +27,14 @@ private:
   float sampleAngle_;
 };
 IViewport *makeViewport() { return new Visuport; }
-Visuport::Visuport() : is_playing_(false), camera_(vec3f(10.f,10.f,5.f), vec3f(0), vec3f(0,0,1)), sampleAngle_(0) {}
+Visuport::Visuport() : is_playing_(false),
+  camera_(vec3f(0.f,-10.f,10.f),
+          vec3f(0),
+          vec3f(0,0,1)), sampleAngle_(0) {}
 void Visuport::init(IViewportController *system) {
   controller_ = system;
   render_ = new Render;
-  ground_ = new Ground(32, 2.f);
+  ground_ = new Ground(8, 8.f);
   central_ = new Central(2, 1.f);
   central_->frame().move(vec3f(0,0,4));
   central_->frame().update();
@@ -45,9 +51,6 @@ void Visuport::resize(vec2i s) {
 
 void Visuport::draw(int ms, float dt) {
   glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-  //float angle = c_pi4 + .2*sin(ms * 1e-3f);
-  //camera_.lookAt(vec3f(cos(angle), sin(angle), .5f) * 10.f, vec3f(0), vec3f(0,0,1));
-  camera_.update();
   
   murth_event_t mevent;
   while (murth_get_event(&mevent) != 0) {
@@ -57,10 +60,6 @@ void Visuport::draw(int ms, float dt) {
       central_->spike(9.f);
       ground_->spike(vec3f(cos(sampleAngle_) * 16.f,
                            sin(sampleAngle_) * 16.f, 6.f));
-      break;
-    case 2:
-        camera_.translate(vec3f(20.f * (mevent.value.i / 127.f - .5f), 6.f, 7.f));
-        camera_.update();
       break;
     }
   }
